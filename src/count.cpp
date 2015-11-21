04a65dd0f5b90228826328368e3fdd9a62612d6f
bharatchekuri <bharatvarma1911@gmail.com>
2015-11-22 00:28:22 +0530

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -11,6 +11,47 @@ ERROR CASES: Return -1 in error cases.
 
 NOTES:
 */
-int count(int num1, int num2) {
-	return 0;
+int count(int num1, int num2) 
+{
+	if (num1 == 0)
+	{
+		return -1;
+	}
+	else if (num2 == 0)
+	{
+		return 0;
+	}
+	else if (num1 > num2 )
+	{
+		return 0;
+		
+	}
+	else if (num1==1)
+	{
+		int c=1, n = num1,i;
+		
+		for (i = num1; i < num2; i = i + num1)
+		{
+		
+				c = c + 1;
+			
+		}
+		return c;
+	}
+	else
+	{
+			int c = 0, n = num1, i;
+
+			for (i = num1; i <= num2; i = i + num1)
+			{
+				
+				
+					c = c + 1;
+				
+			}
+			return c;
+		
+	}
 }
+
+	
